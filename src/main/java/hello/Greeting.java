f7b425e7ccf9ea14c63bac09eee8e8a1d5e33cc9
Kevin O'Leary <beergeekgamer@gmail.com>
2019-02-06 09:41:40 -0600

    Initial commit

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,13 @@
+package hello;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class Greeting {
+	private final long id;
+	private final String content;
+}
